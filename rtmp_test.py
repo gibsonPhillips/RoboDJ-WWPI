e075d78940d9a4981e81f62650876e484ef1e7f5
TheDarkThatBarks <125921991+TheDarkThatBarks@users.noreply.github.com>
2024-01-13 12:39:42 -0500

    Update rtmp_test.py

--- rtmp_test.py
+++ rtmp_test.py
@@ -7,61 +7,61 @@ import tempfile
 from pyrtmp import StreamClosedException
 from pyrtmp.rtmp import RTMPProtocol
 from pyrtmp.session_manager import SessionManager
-from pyrtmp.messages.audio import AudioMessage  
-from pyrtmp.messages.command import NCConnect, NCCreateStream, NSPublish, NSCloseStream, NSDeleteStream  
-from pyrtmp.messages.data import MetaDataMessage  
-from pyrtmp.messages.protocol_control import WindowAcknowledgementSize, SetChunkSize, SetPeerBandwidth  
-from pyrtmp.messages.user_control import StreamBegin  
-from pyrtmp.messages.video import VideoMessage  
-from pyrtmp.flv import FLVWriter, FLVMediaType  
+from pyrtmp.messages.audio import AudioMessage
+from pyrtmp.messages.command import NCConnect, NCCreateStream, NSPublish, NSCloseStream, NSDeleteStream 
+from pyrtmp.messages.data import MetaDataMessage
+from pyrtmp.messages.protocol_control import WindowAcknowledgementSize, SetChunkSize, SetPeerBandwidth
+from pyrtmp.messages.user_control import StreamBegin
+from pyrtmp.messages.video import VideoMessage
+from pyrtmp.flv import FLVWriter, FLVMediaType
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+async def simple_controller(reader, writer):
+    session = SessionManager(reader=reader, writer=writer)
+    flv = None
+    try:
+        logger.debug(f'Client connected {session.peername}')
   
-logging.basicConfig(level=logging.DEBUG)  
-logger = logging.getLogger(__name__)  
-logger.setLevel(logging.DEBUG)  
+        # do handshake
+        await session.handshake()
   
-  
-async def simple_controller(reader, writer):  
-    session = SessionManager(reader=reader, writer=writer)  
-    flv = None  
-    try:  
-        logger.debug(f'Client connected {session.peername}')  
-  
-        # do handshake  
-        await session.handshake()  
-  
-        # read chunks  
-        while True:  
-                chunk = await session.read_chunk_from_stream()  
-                message = chunk.as_message()  
-                logger.debug(f"Receiving {str(message)} {message.chunk_id}")  
-                if isinstance(message, NCConnect):  
-                    session.write_chunk_to_stream(WindowAcknowledgementSize(ack_window_size=5000000))  
-                    session.write_chunk_to_stream(SetPeerBandwidth(ack_window_size=5000000, limit_type=2))  
-                    session.write_chunk_to_stream(StreamBegin(stream_id=0))  
-                    session.write_chunk_to_stream(SetChunkSize(chunk_size=8192))  
-                    session.writer_chunk_size = 8192  
-                    session.write_chunk_to_stream(message.create_response())  
-                    await session.drain()  
-                    logger.debug("Response to NCConnect")  
-                elif isinstance(message, WindowAcknowledgementSize):  
+        # read chunks
+        while True:
+                chunk = await session.read_chunk_from_stream()
+                message = chunk.as_message()
+                logger.debug(f"Receiving {str(message)} {message.chunk_id}")
+                if isinstance(message, NCConnect):
+                    session.write_chunk_to_stream(WindowAcknowledgementSize(ack_window_size=5000000))
+                    session.write_chunk_to_stream(SetPeerBandwidth(ack_window_size=5000000, limit_type=2))
+                    session.write_chunk_to_stream(StreamBegin(stream_id=0))
+                    session.write_chunk_to_stream(SetChunkSize(chunk_size=8192))
+                    session.writer_chunk_size = 8192
+                    session.write_chunk_to_stream(message.create_response())
+                    await session.drain()
+                    logger.debug("Response to NCConnect")
+                elif isinstance(message, WindowAcknowledgementSize):
                     pass  
-                elif isinstance(message, NCCreateStream):  
-                    session.write_chunk_to_stream(message.create_response())  
+                elif isinstance(message, NCCreateStream):
+                    session.write_chunk_to_stream(message.create_response())
                     await session.drain()  
-                    logger.debug("Response to NCCreateStream")  
-                elif isinstance(message, NSPublish):  
-                    # create flv file at temp  
-                    flv = FLVWriter(os.path.join(tempfile.gettempdir(), message.publishing_name))  
-                    session.write_chunk_to_stream(StreamBegin(stream_id=1))  
-                    session.write_chunk_to_stream(message.create_response())  
-                    await session.drain()  
-                    logger.debug("Response to NSPublish")  
-                elif isinstance(message, MetaDataMessage):  
-                    # Write meta data to file  
-                    flv.write(0, message.to_raw_meta(), FLVMediaType.OBJECT)  
+                    logger.debug("Response to NCCreateStream")
+                elif isinstance(message, NSPublish):
+                    # create flv file at temp
+                    flv = FLVWriter(os.path.join(tempfile.gettempdir(), message.publishing_name))
+                    session.write_chunk_to_stream(StreamBegin(stream_id=1))
+                    session.write_chunk_to_stream(message.create_response())
+                    await session.drain()
+                    logger.debug("Response to NSPublish")
+                elif isinstance(message, MetaDataMessage):
+                    # Write meta data to file
+                    flv.write(0, message.to_raw_meta(), FLVMediaType.OBJECT)
                 elif isinstance(message, SetChunkSize):  
                     session.reader_chunk_size = message.chunk_size  
-                elif isinstance(message, VideoMessage):  
+                elif isinstance(message, VideoMessage):
                     # Write video data to file  
                     flv.write(message.timestamp, message.payload, FLVMediaType.VIDEO)  
                 elif isinstance(message, AudioMessage):  
@@ -69,7 +69,7 @@ async def simple_controller(reader, writer):
                     flv.write(message.timestamp, message.payload, FLVMediaType.AUDIO)  
                 elif isinstance(message, NSCloseStream):  
                     pass  
-                elif isinstance(message, NSDeleteStream):  
+                elif isinstance(message, NSDeleteStream):
                     pass
                 else:  
                     logger.debug(f"Unknown message {str(message)}")

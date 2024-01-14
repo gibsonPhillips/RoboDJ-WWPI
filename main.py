2f4b90574a24835f1654eb04c3a387083dc873b7
Gibson <gcaryp4@gmail.com>
2024-01-14 01:24:54 -0500

    objectifying stuff with main

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-
+import vlc
 import SongPicker
+import time
+import os
+os.add_dll_directory(os.getcwd())
 
-
-def print_hi():
+def main():
     #p = vlc.MediaPlayer("/resources\\Benjamin Lee - Sleeping (Lofi).mp3")
 
     #p.play()
-    songs = SongPicker.getSongs()
-    nextSong = SongPicker.nextSong(songs)
-    print(nextSong)
-    nextSongPath = SongPicker.getSongPath(nextSong)
-    print(nextSongPath)
+
+    sPicker = SongPicker.Songs()
+
+    while True:
+        # just the title of the song
+        nextSong = sPicker.nextSong()
+        # print(nextSong)
+
+        # just gets the path to the song
+        nextSongPath = sPicker.getSongPath(nextSong)
+        print(nextSongPath)
+
+        # wack
+        time.sleep(3)
+
+def vlc_test():
+    song = vlc.MediaPlayer("resources/Benjamin Lee - Sleeping (Lofi).mp3")
+    song.play()
+    time.sleep(8)
 
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    vlc_test()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
